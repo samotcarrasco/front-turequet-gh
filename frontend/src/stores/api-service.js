411ab9441f13424b7bfa256898efefab030bc8da
samotcarrasco <samotcarrasco@gmail.com>
2023-06-19 23:15:40 +0200

    corregido error adquirir material

--- frontend/src/stores/api-service.js
+++ frontend/src/stores/api-service.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-//export const host = "https://truequet-carrascodim.b4a.run/"
-export const host = "http://localhost:8080/"
+export const host = "https://truequet-carrascodim.b4a.run/"
 
 export function llamadaAPI(method, body, path) {
   let config = {
